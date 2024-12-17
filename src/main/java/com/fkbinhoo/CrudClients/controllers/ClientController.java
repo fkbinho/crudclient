0d23a1595e1d98c96e1b4963cd1d976fce6a2a31
fkbinho <sir_kleber@hotmail.com>
2024-12-16 22:13:31 -0300

    implement findAll pageable

--- src/main/java/com/fkbinhoo/CrudClients/controllers/ClientController.java
+++ src/main/java/com/fkbinhoo/CrudClients/controllers/ClientController.java
@@ -1,11 +1,13 @@
 package com.fkbinhoo.CrudClients.controllers;
 
+import com.fkbinhoo.CrudClients.dto.ClientDTO;
 import com.fkbinhoo.CrudClients.entities.Client;
 import com.fkbinhoo.CrudClients.repositories.ClientRepository;
 import com.fkbinhoo.CrudClients.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +19,9 @@ public class ClientController {
     @Autowired
     ClientService clientService;
 
-    @Autowired
-    ClientRepository clientRepository;
-
     @GetMapping
-    public Page<Client> findAll(Pageable pageable) {
-        return clientRepository.findAll(pageable);
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> clientDTO = clientService.findAll(pageable);
+        return ResponseEntity.ok(clientDTO);
     }
 }
--- src/main/java/com/fkbinhoo/CrudClients/services/ClientService.java
+++ src/main/java/com/fkbinhoo/CrudClients/services/ClientService.java
@@ -1,8 +1,13 @@
 package com.fkbinhoo.CrudClients.services;
 
+import com.fkbinhoo.CrudClients.dto.ClientDTO;
+import com.fkbinhoo.CrudClients.entities.Client;
 import com.fkbinhoo.CrudClients.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientService {
@@ -10,5 +15,10 @@ public class ClientService {
     @Autowired
     ClientRepository clientRepository;
 
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = clientRepository.findAll(pageable);
 
+        return result.map(x -> new ClientDTO());
+    }
 }
