3fc7be03671ad1acd73a3dc66af4a26ea7df130e
fkbinho <sir_kleber@hotmail.com>
2024-12-16 21:01:46 -0300

    project created

---
+++ src/main/java/com/fkbinhoo/CrudClients/CrudClientsApplication.java
@@ -0,0 +1,13 @@
+package com.fkbinhoo.CrudClients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClientsApplication.class, args);
+	}
+
+}
