da81587327fa74086764cf8fdd7f611bdb3b7a2e
fkbinho <sir_kleber@hotmail.com>
2024-12-19 12:20:20 -0300

    add insert

--- src/main/java/com/fkbinhoo/CrudClients/controllers/ClientController.java
+++ src/main/java/com/fkbinhoo/CrudClients/controllers/ClientController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -27,6 +24,12 @@ public class ClientController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<ClientDTO> clientById(@PathVariable Long id) {
         ClientDTO clientDTO = clientService.clientById(id);
+        return ResponseEntity.ok(clientDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert (@RequestBody ClientDTO clientDTO) {
+        clientDTO= clientService.insert(clientDTO);
         return ResponseEntity.ok(clientDTO);
     }
 }
--- src/main/java/com/fkbinhoo/CrudClients/services/ClientService.java
+++ src/main/java/com/fkbinhoo/CrudClients/services/ClientService.java
@@ -29,4 +29,19 @@ public class ClientService {
         Client client = clientRepository.findById(id).get();
         return new ClientDTO(client);
     }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO clientDTO) {
+        Client client = new Client();
+
+        client.setName(clientDTO.getName());
+        client.setCpf(clientDTO.getCpf());
+        client.setIncome(clientDTO.getIncome());
+        client.setBirthDate(clientDTO.getBirthDate());
+        client.setChildren(client.getChildren());
+
+        client = clientRepository.save(client);
+
+        return new ClientDTO(client);
+    }
 }
